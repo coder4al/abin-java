18ffa13275526ffcc1ffa5f8269db04870cc8696
coder4al <coder4al@gmail.com>
2024-09-26 13:48:36 +0530

    IsFibPrime done

---
+++ IsFibPrime.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class IsFibPrime {
+
+    public static void main(String[] args) {
+        int n = 50, prev = 1, curr = 2;
+
+        while (curr <= n) {
+            if (curr == 2 || curr == 3 || (curr * curr - 1) % 24 == 0)
+                System.out.print(curr + " ");
+            curr = prev + (prev = curr);
+        }
+    }
+}
